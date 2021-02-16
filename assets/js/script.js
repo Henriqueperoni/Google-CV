ea01add46debf7be7b7973863efe2950672e5371
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-16 11:16:52 +0000

    Initial style added to images page

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,10 @@ const navSlide = () => {
 
 navSlide()
 
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
 
 
 
